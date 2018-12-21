509d81350b990c90c6632926f5975783c0df3f79
AmineDev1 <45659795+AmineDev1@users.noreply.github.com>
2018-12-21 12:12:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` KSA Community Forever ♥  `,"http://twitch.tv/S-F")
+client.user.setGame(` [HX1] Servers  `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
